204b985f1057c55dd923db997ffd1efb5569149f
Levent KIRIMGERİ <nomengo667@gmail.com>
2019-11-29 11:57:19 -0800

    Second Entry(Infinite Road)

---
+++ Assets/Scripts/InfiniteRoadGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiniteRoadGenerator : MonoBehaviour
+{
+    public GameObject Road;
+    public GameObject Player;
+    public int Sayaç;
+    
+
+    
+    void Start()
+    {
+        Sayaç = 1;
+      
+    }
+
+    
+    void Update()
+    {
+            if (Player.transform.position.z >= 40*Sayaç)
+            {
+            Sayaç = Sayaç + 1;
+            Instantiate(Road, new Vector3(0, 0, 40 * Sayaç),Quaternion.identity);
+            }
+        
+    }
+    
+}
