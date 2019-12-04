c9df9415b1be29918d280d0298d57aa2ea573b41
Levent KIRIMGERİ <nomengo667@gmail.com>
2019-12-04 07:55:55 -0800

    Fourth Entry(spawn and remove)

--- Assets/Scripts/InfiniteRoadGenerator.cs
+++ Assets/Scripts/InfiniteRoadGenerator.cs
@@ -2,30 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class InfiniteRoadGenerator : MonoBehaviour
 {
-    public GameObject Road;
+    public GameObject Road1;
+    public GameObject Road2;
     public GameObject Player;
     private int Sayaç;
+    private int Hak;
     
+
+    private GameObject instanceCache;
 
     
     void Start()
     {
-        
         Sayaç = 1;
-      
+        Hak = 1;
     }
 
     
     void Update()
-    {
-            if (Player.transform.position.z >= 40*Sayaç)
-            {
-            Sayaç = Sayaç + 1;
-            Instantiate(Road, new Vector3(0, 0, 40 * Sayaç),Quaternion.identity);
-            }
+    { 
         
+          if (Player.transform.position.z >= 40 * Sayaç)
+           {
+            Sayaç = Sayaç + 1;
+            Destroy(instanceCache,8);
+            instanceCache = Instantiate(Road1, new Vector3(0, 0, 40 * Sayaç), Quaternion.identity);
+           }
+          if(Player.transform.position.z >= 60 * Hak)
+        {
+            Hak = Hak + 1;
+            Destroy(instanceCache,8);
+            instanceCache = Instantiate(Road2, new Vector3(0, 0, 60 * Hak), Quaternion.identity);
+        }
+       
     }
     
 }
